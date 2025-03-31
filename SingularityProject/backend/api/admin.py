333fe0ec6681aa571b72309acc6d30711656d6d3
AidAliu <aidaliu50@gmail.com>
2025-03-31 21:42:22 +0200

    Paper model created with the user and project many to many connection to it

--- SingularityProject/backend/api/admin.py
+++ SingularityProject/backend/api/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from .models import User
 from .models import Project
 from .models import UserProject
+from .models import Paper
+from .models import PaperProject
+from .models import UserPaper
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Project)
 admin.site.register(UserProject)
+admin.site.register(Paper)
+admin.site.register(PaperProject)
+admin.site.register(UserPaper)
+
--- SingularityProject/backend/api/models/__init__.py
+++ SingularityProject/backend/api/models/__init__.py
@@ -1,3 +1,6 @@
 from .user import User
 from .project import Project
 from .user_project import UserProject
+from .paper import Paper
+from .paper_project import PaperProject
+from .user_paper import UserPaper
