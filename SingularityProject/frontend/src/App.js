e8e7227873d69edf87c02f1f462f25deb85f0590
Arb Sahatqija <as67822@ubt-uni.net>
2025-05-13 14:59:55 +0200

    Resolved merge conflict in App.js

--- SingularityProject/frontend/src/App.js
+++ SingularityProject/frontend/src/App.js
@@ -25,59 +25,55 @@ import Profile from './components/userProfile/profile';
 import EditProfile from './components/userProfile/editProfile';
 import AboutMe from './components/aboutMe/aboutMe';
 import ProjectCard from './components/projectCard/projectCard';
-<<<<<<< HEAD
-import Home from './components/home/Home';    
-=======
+import Home from './components/home/Home';
 import CommunicationHub from './pages/CommunicationHub';
->>>>>>> a60f87a69be228d769816b12956f02e0e1645baa
 
 // Paper pages
 import PaperPage from './pages/auth/dashboard/paper/PaperPage';
 import PaperForm from './pages/auth/dashboard/paper/PaperForm';
+
 // Paper-Project pages
 import PaperProjectPage from './pages/auth/dashboard/paper_project/PaperProjectPage';
 import PaperProjectForm from './pages/auth/dashboard/paper_project/PaperProjectForm';
 
-//Project pages
+// Project pages
 import ProjectPage from './pages/auth/dashboard/project/ProjectPage';
 import ProjectForm from './pages/auth/dashboard/project/ProjectForm';
 
-//Tutorial Pages
+// Tutorial Pages
 import TutorialPage from './pages/auth/dashboard/tutorial/TutorialPage';
 import TutorialForm from './pages/auth/dashboard/tutorial/TutorialForm';
 
-//Review Pages
+// Review Pages
 import ReviewPage from './pages/auth/dashboard/review/ReviewPage';
 import ReviewForm from './pages/auth/dashboard/review/ReviewForm';
 
-//Friendship Pages
+// Friendship Pages
 import FriendshipPage from './pages/auth/dashboard/friendship/FriendshipPage';
 import FriendshipForm from './pages/auth/dashboard/friendship/FriendshipForm';
 
-//Invitation Pages
+// Invitation Pages
 import InvitationPage from './pages/auth/dashboard/invitation/InvitationPage';
 import InvitationForm from './pages/auth/dashboard/invitation/InvitationForm';
 
-//User Pages
+// User-Paper Pages
 import UserPaperForm from './pages/auth/dashboard/user_paper/UserPaperForm';
 import UserPaperPage from './pages/auth/dashboard/user_paper/UserPaperPage';
 
-//Roles Pages
+// Required Roles Pages
 import RequiredrolesPage from './pages/auth/dashboard/requirement_roles/RequiredrolesPage';
 import RequiredrolesForm from './pages/auth/dashboard/requirement_roles/RequiredrolesForm';
 
-//User-Project Pages
+// User-Project Pages
 import UserProjectPage from './pages/auth/dashboard/user_project/UserProjectPage';
 import UserProjectForm from './pages/auth/dashboard/user_project/UserProjectForm';
 
-
-// ─── Layout wrapper that shows the NavigationBar on every protected page ──
 function LayoutWithNav() {
   return (
     <>
       <NavigationBar />
       <div className="pt-20 px-6">
-        <Outlet /> {/* nested routes rendered here */}
+        <Outlet />
       </div>
     </>
   );
@@ -88,101 +84,72 @@ function App() {
     <NotificationProvider>
       <Router>
         <Routes>
-          {/* Public auth routes (no navbar) */}
+          {/* Public auth routes */}
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
 
-            {/* Staff-only routes */}
-            <Route element={<RequireStaff><Outlet/></RequireStaff>}>
-            {/* Top‐level dashboard overview */}
+          {/* Staff-only routes */}
+          <Route element={<RequireStaff><Outlet /></RequireStaff>}>
             <Route path="/dashboard" element={<Dashboard />} />
-
-            {/* Users CRUD */}
             <Route path="/dashboard/users" element={<UsersPage />} />
             <Route path="/dashboard/users/new" element={<UserForm />} />
             <Route path="/dashboard/users/edit/:userId" element={<UserForm />} />
 
-            {/* Paper CRUD */}
             <Route path="/dashboard/papers" element={<PaperPage />} />
             <Route path="/dashboard/papers/new" element={<PaperForm />} />
             <Route path="/dashboard/papers/edit/:paperId" element={<PaperForm />} />
 
-            {/* Paper-Project CRUD */}
             <Route path="/dashboard/paper_projects" element={<PaperProjectPage />} />
             <Route path="/dashboard/paper_projects/new" element={<PaperProjectForm />} />
             <Route path="/dashboard/paper_projects/edit/:paperProjectId" element={<PaperProjectForm />} />
 
-            {/* Project CRUD */}
             <Route path="/dashboard/projects" element={<ProjectPage />} />
             <Route path="/dashboard/projects/new" element={<ProjectForm />} />
-            <Route path="/dashboard/projects/edit/:projectID" element={<ProjectForm />} />   
+            <Route path="/dashboard/projects/edit/:projectID" element={<ProjectForm />} />
 
-            {/* Tutorial CRUD */}
             <Route path="/dashboard/tutorials" element={<TutorialPage />} />
             <Route path="/dashboard/tutorials/new" element={<TutorialForm />} />
-            <Route path="/dashboard/tutorials/edit/:TutorialID" element={<TutorialForm />} /> 
+            <Route path="/dashboard/tutorials/edit/:TutorialID" element={<TutorialForm />} />
 
-            {/* Review CRUD */}
             <Route path="/dashboard/reviews" element={<ReviewPage />} />
             <Route path="/dashboard/reviews/new" element={<ReviewForm />} />
             <Route path="/dashboard/reviews/edit/:ReviewID" element={<ReviewForm />} />
-            
-            {/* Friendship CRUD */}
+
             <Route path="/dashboard/friendships" element={<FriendshipPage />} />
             <Route path="/dashboard/friendships/new" element={<FriendshipForm />} />
             <Route path="/dashboard/friendships/edit/:friendshipId" element={<FriendshipForm />} />
-            
-            {/* Invitation CRUD */}
+
             <Route path="/dashboard/invitations" element={<InvitationPage />} />
             <Route path="/dashboard/invitations/new" element={<InvitationForm />} />
             <Route path="/dashboard/invitations/edit/:invitationId" element={<InvitationForm />} />
 
-            {/* === USER PAPERS === */}
-            <Route path="/dashboard/user_papers"               element={<UserPaperPage />} />
-            <Route path="/dashboard/user_papers/new"           element={<UserPaperForm />} />
-            <Route path="/dashboard/user_papers/edit/:userPaperId"      element={<UserPaperForm />} />
+            <Route path="/dashboard/user_papers" element={<UserPaperPage />} />
+            <Route path="/dashboard/user_papers/new" element={<UserPaperForm />} />
+            <Route path="/dashboard/user_papers/edit/:userPaperId" element={<UserPaperForm />} />
 
-            {/* === Required roles === */}
-            <Route path="/dashboard/required_roles"               element={<RequiredrolesPage />} />
-            <Route path="/dashboard/required_roles/new"           element={<RequiredrolesForm />} />
-            <Route path="/dashboard/required_roles/edit/:RequiredrolesID"      element={<RequiredrolesForm />} />
-          
-            {/* === USER Projects === */}
-            <Route path="/dashboard/user_projects"    element={<UserProjectPage />} />
-            <Route path="/dashboard/user_projects/new"   element={<UserProjectForm />} />
+            <Route path="/dashboard/required_roles" element={<RequiredrolesPage />} />
+            <Route path="/dashboard/required_roles/new" element={<RequiredrolesForm />} />
+            <Route path="/dashboard/required_roles/edit/:RequiredrolesID" element={<RequiredrolesForm />} />
+
+            <Route path="/dashboard/user_projects" element={<UserProjectPage />} />
+            <Route path="/dashboard/user_projects/new" element={<UserProjectForm />} />
             <Route path="/dashboard/user_projects/edit/:userProjectId" element={<UserProjectForm />} />
-
-          
           </Route>
 
-
-
-<<<<<<< HEAD
-        {/* All other routes that need the NavigationBar */}
-        <Route element={<LayoutWithNav />}>
-          <Route path="profile" element={<Profile />} />
-          <Route path="edit-profile" element={<EditProfile />} />
-          <Route path="about" element={<AboutMe />} />
-          <Route path="card" element={<ProjectCard />} />
-          <Route path="home" element={<Home />} />
-        </Route>
-      </Routes>
-    </Router>
-=======
-          {/* All other routes that need the NavigationBar */}
+          {/* All other routes with navbar */}
           <Route element={<LayoutWithNav />}>
             <Route path="profile" element={<Profile />} />
             <Route path="edit-profile" element={<EditProfile />} />
             <Route path="about" element={<AboutMe />} />
             <Route path="card" element={<ProjectCard />} />
+            <Route path="home" element={<Home />} />
             <Route path="communication" element={<CommunicationHub />} />
           </Route>
         </Routes>
       </Router>
     </NotificationProvider>
->>>>>>> a60f87a69be228d769816b12956f02e0e1645baa
   );
 }
 
